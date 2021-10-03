acee76d5ffc67431d4b0cd1feb0e9318504ae001
Michel Ribeiro <michel.ribeiro1@hotmail.com>
2021-10-03 19:26:19 -0300

    feature: use functions

--- src/cpf-validator.js
+++ src/cpf-validator.js
@@ -1,74 +1,77 @@
-export default class CPFValidator {
-  static validate(cpf) {
-    if (!CPFValidator.isCpfDataTypeValid(cpf)) {
-      return false;
-    }
+const CPF_VALID_LENGTH = 11;
 
-    if (!CPFValidator.isCpfLengthValid(cpf)) {
-      return false;
-    }
-
-    const normalizedCpf = CPFValidator.normalizeCpf(cpf);
-
-    if (CPFValidator.isAllDigitsEquals(normalizedCpf)) {
-      return false;
-    }
-
-    const cpfWithoutCheckerDigits = normalizedCpf.slice(0, 9);
-
-    const firstCheckerDigit = CPFValidator.calculateCheckerDigit(cpfWithoutCheckerDigits);
-
-    const cpfWithOneCheckerDigit = [...cpfWithoutCheckerDigits, firstCheckerDigit];
-
-    const secondCheckerDigit = CPFValidator.calculateCheckerDigit(
-      cpfWithOneCheckerDigit,
-    );
-
-    const firstCheckerDigitToValidate = normalizedCpf[normalizedCpf.length - 2];
-    const secondCheckerDigitToValidate = normalizedCpf[normalizedCpf.length - 1];
-
-    return firstCheckerDigit === firstCheckerDigitToValidate
-      && secondCheckerDigit === secondCheckerDigitToValidate;
-  }
-
-  static isCpfDataTypeValid(cpf) {
-    return typeof cpf === 'string';
-  }
-
-  static isCpfLengthValid(cpf) {
-    return cpf.length >= 11 || cpf.length <= 14;
-  }
-
-  static normalizeCpf(cpf) {
-    return cpf
-      .replace('.', '')
-      .replace('.', '')
-      .replace('-', '')
-      .replace(' ', '')
-      .split('')
-      .map((digit) => parseInt(digit, 10));
-  }
-
-  static isAllDigitsEquals(cpf) {
-    return cpf.every((digit) => digit === cpf[0]);
-  }
-
-  static calculateCheckerDigit(cpfDigits = []) {
-    const factors = this.generateDigitsFactors(cpfDigits);
-    const CPF_LENGTH = 11;
-
-    const digitsSum = cpfDigits.reduce(
-      (_digitsSum, digit, index) => _digitsSum + (digit * factors[index]),
-      0,
-    );
-
-    const digitCheckerFactor = digitsSum % CPF_LENGTH;
-
-    return digitCheckerFactor < 2 ? 0 : CPF_LENGTH - digitCheckerFactor;
-  }
-
-  static generateDigitsFactors(cpfDigits = []) {
-    const FIRST_FACTOR = cpfDigits.length === 9 ? 10 : 11;
-    return cpfDigits.map((_, index) => FIRST_FACTOR - index);
-  }
+function isCpfDataTypeValid(cpf) {
+  return typeof cpf === 'string';
 }
+
+function isCpfLengthValid(cpf) {
+  return cpf.length === 11;
+}
+
+function normalizeCpf(cpf) {
+  return cpf
+    .replace(/\D/gu, '')
+    .split('')
+    .map((digit) => parseInt(digit, 10));
+}
+
+function isAllDigitsEquals(cpf) {
+  return cpf.every((digit) => digit === cpf[0]);
+}
+
+function generateDigitsFactors(cpfDigits, firstFactor) {
+  return cpfDigits.map((_, index) => firstFactor - index);
+}
+
+function calculateCheckerDigit(cpfDigits, firstFactor) {
+  const factors = generateDigitsFactors(cpfDigits, firstFactor);
+
+  const digitsSum = cpfDigits.reduce(
+    (_digitsSum, digit, index) => _digitsSum + (digit * factors[index]),
+    0,
+  );
+
+  const digitCheckerFactor = digitsSum % CPF_VALID_LENGTH;
+
+  return digitCheckerFactor < 2 ? 0 : CPF_VALID_LENGTH - digitCheckerFactor;
+}
+
+function validate(cpf) {
+  if (!isCpfDataTypeValid(cpf)) {
+    return false;
+  }
+
+  const normalizedCpf = normalizeCpf(cpf);
+
+  if (!isCpfLengthValid(normalizedCpf)) {
+    return false;
+  }
+
+  if (isAllDigitsEquals(normalizedCpf)) {
+    return false;
+  }
+
+  const [cpfWithoutCheckerDigits, checkerDigits] = [
+    normalizedCpf.slice(0, 9),
+    normalizedCpf.slice(9, 11),
+  ];
+
+  const expectedFirstCheckerDigit = calculateCheckerDigit(cpfWithoutCheckerDigits, 10);
+
+  const cpfWithOneCheckerDigit = [...cpfWithoutCheckerDigits, checkerDigits[0]];
+
+  const expectedSecondCheckerDigit = calculateCheckerDigit(
+    cpfWithOneCheckerDigit,
+    11,
+  );
+
+  const firstCheckerDigitToValidate = normalizedCpf[normalizedCpf.length - 2];
+  const secondCheckerDigitToValidate = normalizedCpf[normalizedCpf.length - 1];
+
+  return expectedFirstCheckerDigit === firstCheckerDigitToValidate
+    && expectedSecondCheckerDigit === secondCheckerDigitToValidate;
+}
+
+export default {
+  validate,
+};
