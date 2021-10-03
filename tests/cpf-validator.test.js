083382fd9610f7a81b2773db3323042ec4b3cad7
Michel Ribeiro <michel.ribeiro1@hotmail.com>
2021-10-03 19:26:40 -0300

    feature: import new function

--- tests/cpf-validator.test.js
+++ tests/cpf-validator.test.js
@@ -1,35 +1,37 @@
-import CPFValidator from '../src/cpf-validator';
+import cpfValidator from '../src/cpf-validator';
+
+// TODO: Melhorar nomes dos testes
 
 describe('validate cpf', () => {
   it('should return TRUE for 935.411.347-80', () => {
-    expect(CPFValidator.validate('935.411.347-80')).toBe(true);
+    expect(cpfValidator.validate('935.411.347-80')).toBe(true);
   });
 
   it('should return TRUE for 93541134780', () => {
-    expect(CPFValidator.validate('93541134780')).toBe(true);
+    expect(cpfValidator.validate('93541134780')).toBe(true);
   });
 
   it('should return FALSE for 111.111.111-11', () => {
-    expect(CPFValidator.validate('111.111.111-11')).toBe(false);
+    expect(cpfValidator.validate('111.111.111-11')).toBe(false);
   });
 
   it('should return FALSE for 11111111111', () => {
-    expect(CPFValidator.validate('11111111111')).toBe(false);
+    expect(cpfValidator.validate('11111111111')).toBe(false);
   });
 
   it('should return FALSE for 2345678999', () => {
-    expect(CPFValidator.validate('2345678999')).toBe(false);
+    expect(cpfValidator.validate('2345678999')).toBe(false);
   });
 
   it('should return FALSE for NULL', () => {
-    expect(CPFValidator.validate(null)).toBe(false);
+    expect(cpfValidator.validate(null)).toBe(false);
   });
 
   it('should return FALSE for 12345', () => {
-    expect(CPFValidator.validate('12345')).toBe(false);
+    expect(cpfValidator.validate('12345')).toBe(false);
   });
 
   it('should return FALSE for 12345notanumber', () => {
-    expect(CPFValidator.validate('12345notanumber')).toBe(false);
+    expect(cpfValidator.validate('12345notanumber')).toBe(false);
   });
 });
